7e4dd015cb0bd91c27b6312fef2adf28d581d7aa
Akriti1701 <aakritisingh1789@gmail.com>
2024-07-03 17:05:40 +0530

    Update portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Akriti Kumari",
+  title: "Web Developer",
+  email: "akriti@example.com",
+  gitHub: "Akriti1701",
+  instagram: "_akrriti_",
+  linkedIn: "akriti1701",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "Akriti1701",
+  youTube: "@akriti1701",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/portfolio_img.avif";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,28 +31,29 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Digital Restaurant Menu 🍜",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
-  },
-  {
-    title: "Web Development for Beginners",
-    description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Digital restaurant menu website enabling schools to filter and select dishes based on categories or prices.",
+    url: "hhttps://best-bites-ecru.vercel.app/",
   },
   {
     title: "My Resume Site",
     description:
       "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+    url: "https://github.com/Akriti1701/Resume",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Product page design",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "One-page product design showcasing top female fashion items, scalable for broader clothing sales or as a robust ecommerce platform.",
+    url: "https://github.com/Akriti1701/ProductPage",
+  },
+
+  {
+    title: "To-do App",
+    description:
+      "A React-Redux application allowing users to add, remove, and mark tasks as completed or pending, with a user-friendly interface for managing task lists.",
+    url: "https://github.com/Akriti1701/React-Redux-Todo-App",
   },
 ];
 
